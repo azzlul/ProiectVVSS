7b3d897c28cd24773226c8459404099a2f3b72cd
ariiuuu <147334646+ariiuuu@users.noreply.github.com>
2026-03-04 14:16:28 +0200

    Initial commit cu proiectul MyDrinkShop

---
+++ src/main/java/drinkshop/service/ProductService.java
@@ -0,0 +1,53 @@
+package drinkshop.service;
+
+import drinkshop.domain.*;
+import drinkshop.repository.Repository;
+
+import java.util.List;
+
+public class ProductService {
+
+    private final Repository<Integer, Product> productRepo;
+
+    public ProductService(Repository<Integer, Product> productRepo) {
+        this.productRepo = productRepo;
+    }
+
+    public void addProduct(Product p) {
+        productRepo.save(p);
+    }
+
+    public void updateProduct(int id, String name, double price, CategorieBautura categorie, TipBautura tip) {
+        Product updated = new Product(id, name, price, categorie, tip);
+        productRepo.update(updated);
+    }
+
+    public void deleteProduct(int id) {
+        productRepo.delete(id);
+    }
+
+    public List<Product> getAllProducts() {
+        //modificat
+        return productRepo.findAll();
+    }
+
+    public Product findById(int id) {
+        return productRepo.findOne(id);
+    }
+
+    public List<Product> filterByCategorie(CategorieBautura categorie) {
+        if (categorie == CategorieBautura.ALL) return getAllProducts();
+        return getAllProducts().stream()
+                .filter(p -> p.getCategorie() == categorie)
+                .toList();
+        //modificat toList
+    }
+
+    public List<Product> filterByTip(TipBautura tip) {
+        if (tip == TipBautura.ALL) return getAllProducts();
+        return getAllProducts().stream()
+                .filter(p -> p.getTip() == tip)
+                .toList();
+        //modificat toList
+    }
+}
